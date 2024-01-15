fb4ae52ccf12f2c97514a6da0b954c27b238b752
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-15 13:49:51 +0100

    fix(icon paths): Updated icon paths to new directory
    - Renamed icon paths from "lib/icons" to "assets/icons" to reflect the new directory structure.
    - Updated the icon paths in the `AppIconsData` class in `lib/src/theme/data/icons.dart`.
    - Added the new icon directory to the `assets` section in `pubspec.yaml`.

--- lib/src/theme/data/icons.dart
+++ lib/src/theme/data/icons.dart
@@ -3,14 +3,15 @@ import 'package:flutter_svg/svg.dart' show SvgPicture;
 class AppIconsData {
   const AppIconsData._();
 
-  static SvgPicture date = SvgPicture.asset("lib/icons/date.svg");
-  static SvgPicture role = SvgPicture.asset("lib/icons/role.svg");
-  static SvgPicture classe = SvgPicture.asset("lib/icons/classe.svg");
-  static SvgPicture modifier = SvgPicture.asset("lib/icons/modifier.svg");
-  static SvgPicture detail = SvgPicture.asset("lib/icons/detail.svg");
-  static SvgPicture archiver = SvgPicture.asset("lib/icons/archiver.svg");
-  static SvgPicture rechercher = SvgPicture.asset("lib/icons/rechercher.svg");
-  static SvgPicture absent = SvgPicture.asset("lib/icons/absent.svg");
-  static SvgPicture filtre = SvgPicture.asset("lib/icons/filtre.svg");
-  static SvgPicture attention = SvgPicture.asset("lib/icons/attention.svg");
+  static SvgPicture date = SvgPicture.asset("assets/icons/date.svg");
+  static SvgPicture role = SvgPicture.asset("assets/icons/role.svg");
+  static SvgPicture classe = SvgPicture.asset("assets/icons/classe.svg");
+  static SvgPicture modifier = SvgPicture.asset("assets/icons/modifier.svg");
+  static SvgPicture detail = SvgPicture.asset("assets/icons/detail.svg");
+  static SvgPicture archiver = SvgPicture.asset("assets/icons/archiver.svg");
+  static SvgPicture rechercher =
+      SvgPicture.asset("assets/icons/rechercher.svg");
+  static SvgPicture absent = SvgPicture.asset("assets/icons/absent.svg");
+  static SvgPicture filtre = SvgPicture.asset("assets/icons/filtre.svg");
+  static SvgPicture attention = SvgPicture.asset("assets/icons/attention.svg");
 }
