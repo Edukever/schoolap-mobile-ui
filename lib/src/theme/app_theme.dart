4e53c2fecd36b1eee4f0da6460dc29d526fbe7b0
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-15 13:37:11 +0100

    Refactor theme colors and add new icons
    Refactor scaffold background color and add new SVG icons for visual enhancements and consistency across the app. This will improve the overall visual appeal and maintain uniformity. No associated issues.

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -28,5 +28,6 @@ class AppTheme {
         ),
       ),
     ),
+    scaffoldBackgroundColor: AppColorsData.blanc,
   );
 }
--- lib/src/theme/data/icons.dart
+++ lib/src/theme/data/icons.dart
@@ -1,3 +1,16 @@
+import 'package:flutter_svg/svg.dart' show SvgPicture;
+
 class AppIconsData {
   const AppIconsData._();
+
+  static SvgPicture date = SvgPicture.asset("lib/icons/date.svg");
+  static SvgPicture role = SvgPicture.asset("lib/icons/role.svg");
+  static SvgPicture classe = SvgPicture.asset("lib/icons/classe.svg");
+  static SvgPicture modifier = SvgPicture.asset("lib/icons/modifier.svg");
+  static SvgPicture detail = SvgPicture.asset("lib/icons/detail.svg");
+  static SvgPicture archiver = SvgPicture.asset("lib/icons/archiver.svg");
+  static SvgPicture rechercher = SvgPicture.asset("lib/icons/rechercher.svg");
+  static SvgPicture absent = SvgPicture.asset("lib/icons/absent.svg");
+  static SvgPicture filtre = SvgPicture.asset("lib/icons/filtre.svg");
+  static SvgPicture attention = SvgPicture.asset("lib/icons/attention.svg");
 }
