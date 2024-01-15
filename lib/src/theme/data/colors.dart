8830f3ebdc52aba035b5c2f1e8b82d083c33b2ac
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-15 11:54:55 +0100

    Add color constants for app theme
    This introduces a new file "colors.dart" with color constants for various elements in the app theme,

---
+++ lib/src/theme/data/colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/rendering.dart' show Color;
+
+class AppColorsData {
+  const AppColorsData._();
+  // red color
+  static const Color rouge = Color(0xFFFF4F4F);
+  static const Color rougeLight = Color(0xFFFFE1E1);
+
+  // black color
+  static const Color noir = Color(0xFF292D32);
+  static const Color grid3 = Color(0xFF4E4E4E);
+  static const Color grid2 = Color(0xFFAAAAAA);
+  static const Color grid1 = Color(0xFFE3E3E3);
+  static const Color gridLight = Color(0xFFF6F6F6);
+
+  // white
+  static const Color blanc = Color(0xFFFFFFFF);
+
+  // green color
+  static const Color vert = Color(0xFF1EA951);
+  static const Color vertLight = Color(0xFFDDF3D6);
+
+  // blue color
+  static const Color bleu = Color(0xFF41A3DF);
+  static const Color bleuLight = Color(0xFFE5F9FF);
+  static const Color blue1 = Color(0xFFAEDAEE);
+
+  // yellow color
+  static const Color jaune = Color(0xFFF2B200);
+  static const Color jauneLight = Color(0xFFFFF4D8);
+
+  // orange color
+  static const Color orange = Color(0xFFF68C2C);
+  static const Color orange1 = Color(0xFFFFB390);
+  static const Color orangeLight = Color(0xFFFFF2EC);
+}
