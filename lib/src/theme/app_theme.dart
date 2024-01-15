d5fe247228ac886b92365ce22e4a77bafee52a5c
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-15 13:02:58 +0100

    feat: Add default theme to the application
    Introduce a default application theme with custom configurations for navigation rail, bottom navigation bar, and button styles to maintain a consistent look and feel across the app. This will enhance user experience and visual consistency. Closes #123.

---
+++ lib/src/theme/app_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:schoolap_pkg/src/theme/data/colors.dart';
+
+import 'data/radius.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static final ThemeData defaultTheme = ThemeData(
+    navigationRailTheme: const NavigationRailThemeData(
+      backgroundColor: AppColorsData.blanc,
+      selectedIconTheme: IconThemeData(color: AppColorsData.bleuLight),
+      unselectedIconTheme: IconThemeData(color: AppColorsData.blanc),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedIconTheme: IconThemeData(color: AppColorsData.bleuLight),
+      selectedItemColor: AppColorsData.bleu,
+      unselectedIconTheme: IconThemeData(color: AppColorsData.blanc),
+      unselectedItemColor: AppColorsData.grid1,
+    ),
+    buttonTheme: ButtonThemeData(
+      height: 45,
+      buttonColor: AppColorsData.orange,
+      textTheme: ButtonTextTheme.primary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          const AppRadiusData.primary().small,
+        ),
+      ),
+    ),
+  );
+}
