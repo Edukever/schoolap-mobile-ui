ad6b6b6e07d7cb66ea38e391a685aea0f36133dc
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-18 15:19:09 +0100

    Refactor AppIconButton in app_icon.dart
    The commit refactors the AppIconButton widget in the app_icon.dart file. It introduces several changes such as updating the import statement, modifying enum values, adding new widget properties, and implementing new logic for building the button decoration.
    
    The widget now supports customizable colors, including filled colors, icon colors, and outlined colors. It also allows the button to have a shadow and specifies invalid configurations for outline and square form states.
    
    The build method is updated to return a Container widget instead of a Placeholder widget. The Container widget is now responsible for rendering the button with a specified width, height, and decoration based on the form state.
    
    The icon within the button is now an instance of the Icons.date_range icon and can be customized with different colors.
    
    Overall, these changes enhance the functionality and customization options of the AppIconButton widget.

--- lib/src/widgets/base/app_icon.dart
+++ lib/src/widgets/base/app_icon.dart
@@ -1,33 +1,96 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 enum StateIcon {
   none,
   filled,
-  outlined,
 }
 
 enum StateFormIcon {
   none,
   square,
   circle,
+  outlined,
 }
 
 class AppIconButton extends StatelessWidget {
-  final String? iconData;
-  final StateIcon? state;
+  final List<Color>? colors;
+  final String iconData;
   final StateFormIcon? formState;
   final double? width;
   final double? height;
-  const AppIconButton(
-      {super.key,
-      this.state,
-      this.formState,
-      this.width,
-      this.height,
-      this.iconData});
+  final Color? filledColors;
+  final Color? iconColor;
+  final Color? outlinedColor;
+  final bool hasShadow;
+
+  const AppIconButton({
+    Key? key,
+    this.formState,
+    this.width,
+    this.height,
+    required this.iconData,
+    this.colors,
+    this.filledColors,
+    this.iconColor,
+    this.outlinedColor,
+    this.hasShadow = false,
+  })  : assert(formState != StateFormIcon.outlined || outlinedColor != null,
+            'Invalid configuration: outlinedColor should be provided when formState is only outlined.'),
+        assert(formState != StateFormIcon.square || hasShadow,
+            'Invalid configuration: hasShadow should be true when formState is only square.'),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: width ?? 70,
+      height: height ?? 70,
+      decoration: buildDecoration(formState ?? StateFormIcon.none),
+      child: Icon(
+        Icons.date_range,
+        color: iconColor,
+      ),
+    );
+  }
+
+  BoxDecoration buildDecoration(StateFormIcon formState) {
+    switch (formState) {
+      case StateFormIcon.square:
+        return BoxDecoration(
+          gradient: LinearGradient(
+            begin: const Alignment(-0.00, 1.00),
+            end: const Alignment(0, -1),
+            colors: colors ??
+                const [
+                  Color(0xFF3F97E3),
+                  Color(0xFF43B4D6),
+                ],
+          ),
+          boxShadow: hasShadow == true
+              ? const [
+                  BoxShadow(
+                    color: Color(0xDDB9D4D0),
+                    blurRadius: 10.71,
+                    offset: Offset(0, 4.29),
+                    spreadRadius: 0,
+                  )
+                ]
+              : null,
+        );
+      case StateFormIcon.circle:
+        return const BoxDecoration(
+          color: Color(0xFFE5F9FF),
+          shape: BoxShape.circle,
+        );
+      case StateFormIcon.outlined:
+        return BoxDecoration(
+          border: Border.all(
+            color: outlinedColor ?? const Color(0xFFF68C2C),
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        );
+      default:
+        return const BoxDecoration();
+    }
   }
 }
