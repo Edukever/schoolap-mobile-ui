c19fbad74d3ca937ccadfa8688d9e875cd7561e1
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-18 16:23:45 +0100

    Add AppCard widget for a customizable Flutter card.
    The AppCard widget is a custom card widget that allows the user to customize the background color, box shadows, and child widget. It has a circular border with a radius of 12 and a default box shadow. The widget is designed to be used as a container for other widgets.

---
+++ lib/src/widgets/base/app_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppCard extends StatelessWidget {
+  final Color backgroundColor;
+  final List<BoxShadow>? shadows;
+  final Widget child;
+
+  const AppCard({
+    Key? key,
+    required this.child,
+    this.backgroundColor = Colors.white,
+    this.shadows,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: shadows ??
+            const [
+              BoxShadow(
+                color: Color(0x42CACFCE),
+                blurRadius: 6.18,
+                offset: Offset(0, 6.18),
+                spreadRadius: 0,
+              )
+            ],
+      ),
+      child: child,
+    );
+  }
+}
