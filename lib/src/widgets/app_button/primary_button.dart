e17cad35d56539c866652410347c968808a215b4
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-18 13:03:41 +0100

    Refactor PrimaryButton with icon support
    This commit introduces changes to the PrimaryButton class to add support for displaying an icon. The button can now have an optional icon, which can be positioned on the left or right side of the button's text. The iconPosition property determines the position of the icon, and hasIcon property is used to specify whether the button has an icon or not. The commit also includes improvements to the code organization and readability.

--- lib/src/widgets/app_button/primary_button.dart
+++ lib/src/widgets/app_button/primary_button.dart
@@ -1,30 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:schoolap_pkg/src/widgets/base/app_text.dart';
 
-class PrimaryButton extends StatelessWidget {
-  final Color? color;
+enum IconPosition {
+  left,
+  right,
+}
+
+/// A primary button widget with customizable properties.
+///
+/// The button can have an optional icon, which can be positioned on the left or right side of the button's text.
+/// If `hasIcon` is set to `true`, the icon will be displayed based on the `iconPosition` property.
+/// If `hasIcon` is set to `false`, the `iconPosition` should be null.
+class PrimaryButton<T> extends StatelessWidget {
+  /// The background color of the button.
+  final Color? backgroundColor;
+
+  /// The text displayed on the button.
   final String title;
+
+  /// The height of the button.
   final double? height;
-  const PrimaryButton(
-      {super.key, this.color, required this.title, this.height});
+
+  /// The position of the icon relative to the button's text.
+  final IconPosition? iconPosition;
+
+  /// Determines whether the button has an icon or not.
+  final bool hasIcon;
+
+  /// The Width of the Button
+  final double? width;
+
+  /// the color of the Text
+  final Color? textColor;
+
+  /// Creates a primary button widget.
+  ///
+  /// The [title] parameter is required.
+  /// The [color] parameter can be used to customize the background color of the button.
+  /// The [height] parameter can be used to set the height of the button.
+  /// The [iconPosition] parameter can be used to specify the position of the icon.
+  /// The [hasIcon] parameter determines whether the button has an icon or not.
+  const PrimaryButton({
+    Key? key,
+    this.backgroundColor,
+    required this.title,
+    this.height,
+    this.iconPosition,
+    this.hasIcon = false,
+    this.width,
+    this.textColor,
+  })  : assert(hasIcon || iconPosition == null,
+            'Invalid configuration: iconPosition should be null when hasIcon is false.'),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      width: width ?? double.infinity,
       height: height ?? 50,
-      decoration: ShapeDecoration(
-        color: color ?? const Color(0xFFF68C2C),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.52),
-        ),
+      decoration: BoxDecoration(
+        color: backgroundColor ?? const Color(0xFFF68C2C),
+        borderRadius: BorderRadius.circular(10.52),
       ),
-      child: const Center(
-        child: AppText.title2(
-          'Suivant',
-          color: Colors.white,
-        ),
+      child: Center(
+        child: hasIcon == true
+            ? Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _buildButtonChildren(),
+              )
+            : AppText.title3(
+                title,
+                color: textColor ?? Colors.white,
+              ),
       ),
     );
+  }
+
+  List<Widget> _buildButtonChildren() {
+    final List<Widget> children = [];
+
+    if (iconPosition == IconPosition.left) {
+      children.add(
+        const Padding(
+          padding: EdgeInsets.only(right: 20),
+          child: Icon(
+            Icons.arrow_forward,
+            color: Colors.white,
+          ),
+        ),
+      );
+    }
+
+    children.add(
+      AppText.title2(
+        title,
+        color: Colors.white,
+      ),
+    );
+
+    if (iconPosition == IconPosition.right) {
+      children.add(
+        const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Icon(
+            Icons.arrow_forward,
+            color: Colors.white,
+          ),
+        ),
+      );
+    }
+
+    return children;
   }
 }
---
+++ lib/src/widgets/app_button/state_secondary_button.dart
@@ -0,0 +1,42 @@
+import 'package:schoolap_pkg/schoolap_pkg.dart';
+
+class StateSecondaryButton extends StatelessWidget {
+  final StatePrimaryButtonState state;
+  final double? width;
+  const StateSecondaryButton({super.key, required this.state, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    const height = 45.0;
+    switch (state) {
+      case StatePrimaryButtonState.download:
+        return PrimaryButton(
+          title: "Télécharger",
+          height: height,
+          width: width,
+          textColor: const Color(0xFF41A3DF),
+          backgroundColor: const Color(0xFFE5F9FF),
+        );
+      case StatePrimaryButtonState.validation:
+        return PrimaryButton(
+          title: "Validation",
+          height: height,
+          width: width,
+          textColor: const Color(0xFFFF4F4F),
+          backgroundColor: const Color(0xFFFFE1E1),
+        );
+
+      case StatePrimaryButtonState.delete:
+        return PrimaryButton(
+          title: "Supprimer",
+          height: height,
+          width: width,
+          textColor: const Color(0xFF1EA951),
+          backgroundColor: const Color(0xFFDDF3D6),
+        );
+
+      default:
+        return Container();
+    }
+  }
+}
