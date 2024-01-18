8b6b3cc4b7a519a5802e9c6031a26869034b0279
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-18 16:06:55 +0100

    Exported two new widget classes: AppChip and AppIcon.
    Exported two new widget classes: AppChip and AppIcon.
    
    The commit adds the export statements for 'AppChip' and 'AppIcon' to the 'lib/src/widgets/widget.dart' file. These new widget classes will be available for use in the application.

--- lib/schoolap_pkg.dart
+++ lib/schoolap_pkg.dart
@@ -1,6 +1,6 @@
 library schoolap_pkg;
 
-export 'package:flutter/widgets.dart';
+export 'package:flutter/material.dart';
 
 export 'src/theme/theme.dart';
 export 'src/theme/app_material_theme.dart';
--- lib/src/widgets/widget.dart
+++ lib/src/widgets/widget.dart
@@ -1,3 +1,7 @@
+export 'package:schoolap_pkg/src/widgets/base/app_chip.dart';
+
+export 'package:schoolap_pkg/src/widgets/base/app_icon.dart';
+
 export 'package:schoolap_pkg/src/widgets/app_button/state_primary_button.dart';
 export 'package:schoolap_pkg/src/widgets/app_button/state_secondary_button.dart';
 
