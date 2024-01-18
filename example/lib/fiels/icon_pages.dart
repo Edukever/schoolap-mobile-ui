f52a79063224262e0ea6ca8c014e5f97e63cd756
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-18 16:07:25 +0100

    style: added new UI elements and organized code
    Added new UI elements, modified the app bar's position, and organized the code structure in the IconPage class. The changes include adding a SingleChildScrollView with padding, a column with various UI elements (e.g., AppIconButton, AppText, AppChip), and setting properties for each UI element.

--- example/lib/fiels/icon_pages.dart
+++ example/lib/fiels/icon_pages.dart
@@ -7,9 +7,82 @@ class IconPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      centerTitle: true,
-      title: const AppText.title2("All icon"),
-    ));
+      appBar: AppBar(
+        centerTitle: true,
+        title: const AppText.title2("All icon"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(
+              children: [
+                AppIconButton(
+                  radius: 0,
+                  iconData: AppIconsData.date,
+                  formState: StateFormIcon.square,
+                  hasShadow: true,
+                ),
+                const SizedBox(width: 10),
+                AppIconButton(
+                  iconData: AppIconsData.date,
+                  formState: StateFormIcon.square,
+                  hasShadow: false,
+                ),
+                const SizedBox(width: 10),
+                AppIconButton(
+                    iconData: AppIconsData.date,
+                    formState: StateFormIcon.circle),
+                const SizedBox(width: 10),
+                AppIconButton(
+                    iconData: AppIconsData.date,
+                    formState: StateFormIcon.outlined,
+                    outlinedColor: Colors.orange,
+                    iconColor: AppColorsData.defaultColors().orange),
+                const SizedBox(width: 10),
+                AppIconButton(
+                    iconData: AppIconsData.date,
+                    formState: StateFormIcon.none,
+                    iconColor: AppColorsData.defaultColors().orange),
+              ],
+            ),
+            const AppText.title1("Token"),
+            const SizedBox(height: 10),
+            const AppChip(
+              label: "Chip",
+            ),
+            const SizedBox(height: 10),
+            const AppChip(
+              label: "Inactive",
+              iconOrTextColor: Color(0xFFFF4F4F),
+              appChipType: AppChipType.dot,
+            ),
+            const SizedBox(height: 10),
+            const AppChip(
+              backgroundColor: Color(0xFFDDF3D6),
+              label: "Inactive",
+              iconOrTextColor: Color(0xFF1EA951),
+              appChipType: AppChipType.dot,
+            ),
+            const SizedBox(height: 10),
+            const AppChip(
+              backgroundColor: Color(0xFFDDF3D6),
+              label: "Inactive",
+              iconOrTextColor: Color(0xFF1EA951),
+              appChipType: AppChipType.icon,
+              icon: Icon(Icons.person, size: 15, color: Color(0xFF1EA951)),
+            ),
+            const SizedBox(height: 10),
+            const AppChip(
+              width: 200,
+              backgroundColor: Color(0xFFDDF3D6),
+              label: 'En cours de traitement',
+              iconOrTextColor: Color(0xFF1EA951),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
