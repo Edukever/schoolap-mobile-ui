294fb629d3e3287fefc1f4121f6fad034ead92cb
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-18 13:03:55 +0100

    Update button_page.dart and widget.dart
    - The title of the app bar in button_page.dart has been changed to "All button".
    - The text "Bouton primary" has been updated to use a custom widget AppText.title3.
    - Added new button variations with icons.
    - Added StatePrimaryButton and StateSecondaryButton widgets.
    - Exported StatePrimaryButton, StateSecondaryButton, and AppText from widget.dart.

--- example/lib/fiels/button_page.dart
+++ example/lib/fiels/button_page.dart
@@ -8,7 +8,7 @@ class ButtonPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("All button"),
+        title: const AppText("All button"),
       ),
       body: const SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 20),
@@ -17,10 +17,66 @@ class ButtonPage extends StatelessWidget {
           children: [
             Padding(
               padding: EdgeInsets.symmetric(vertical: 10),
-              child: Text("Bouton primary"),
+              child: AppText.title3("Bouton primary"),
             ),
             PrimaryButton(
               title: 'Suivant',
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: AppText.title3("Bouton primary avec icon gauche"),
+            ),
+            PrimaryButton(
+              title: 'Suivant',
+              hasIcon: true,
+              iconPosition: IconPosition.left,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: AppText.title3("Bouton primary avec icon droite"),
+            ),
+            PrimaryButton(
+              title: 'Suivant',
+              hasIcon: true,
+              iconPosition: IconPosition.right,
+            ),
+            Divider(),
+            StatePrimaryButton(
+              state: StatePrimaryButtonState.download,
+              width: 247,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            StatePrimaryButton(
+              state: StatePrimaryButtonState.delete,
+              width: 247,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            StatePrimaryButton(
+              state: StatePrimaryButtonState.validation,
+              width: 247,
+            ),
+            Divider(),
+            StateSecondaryButton(
+              state: StatePrimaryButtonState.download,
+              width: 247,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            StateSecondaryButton(
+              state: StatePrimaryButtonState.delete,
+              width: 247,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            StateSecondaryButton(
+              state: StatePrimaryButtonState.validation,
+              width: 247,
             ),
           ],
         ),
--- lib/src/widgets/widget.dart
+++ lib/src/widgets/widget.dart
@@ -1,3 +1,8 @@
+export 'package:schoolap_pkg/src/widgets/app_button/state_primary_button.dart';
+export 'package:schoolap_pkg/src/widgets/app_button/state_secondary_button.dart';
+
+export 'package:schoolap_pkg/src/widgets/base/app_text.dart';
+
 export 'package:schoolap_pkg/src/widgets/app_button/primary_button.dart';
 
 export 'package:schoolap_pkg/src/widgets/app_field/app_text_field.dart';
