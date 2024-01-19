9a920ba885770bc4f0d46fe866839c1cd1fd3a06
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-01-19 16:20:26 +0100

    Refactor custom_card.dart UI components and add new card features
    - Refactored UI components in custom_card.dart to use const constructor where applicable.
    - Added a new SchoolAppCardTile widget with a custom appearance.
    - Added a new SchoolAppCardAlert with a warning type.
    - Added a new SchoolapCardPresence widget for displaying presence information.
    - Added a new SCardWithImage widget for writing a communication.
    
    This commit improves code readability and adds new card features to the custom_card.dart file.

--- example/lib/fiels/custom_card.dart
+++ example/lib/fiels/custom_card.dart
@@ -9,22 +9,63 @@ class CustomCard extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Custom Card"),
       ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(height: 10),
-            SchoolAppCardTile(
+            const SizedBox(height: 10),
+            const SchoolAppCardTile(
               constraints: BoxConstraints(maxWidth: double.infinity),
               title: "Card title",
             ),
-            SizedBox(height: 10),
-            SchoolAppCardAlert(
+            const SizedBox(height: 10),
+            SchoolAppCardTile(
+              constraints: const BoxConstraints(maxWidth: double.infinity),
+              title: 'Rapport du présence',
+              subtitle: '11 décembre 2023 à 09:22',
+              subtitleColor: const Color(0xFF41A3DF),
+              subWidget: Opacity(
+                opacity: 0.80,
+                child: Container(
+                  width: 80,
+                  padding: const EdgeInsets.all(8),
+                  margin: const EdgeInsets.only(top: 10),
+                  decoration: ShapeDecoration(
+                    color: const Color(0xFFE5F9FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.37),
+                    ),
+                  ),
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.person,
+                        size: 15,
+                        color: Color(0xFF41A3DF),
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        '26/28',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFF41A3DF),
+                          fontSize: 12,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const SchoolAppCardAlert(
               type: SchoolAppCardAlertType.warning,
               title: "Card alert",
             ),
-            SizedBox(height: 10),
-            Row(
+            const SizedBox(height: 10),
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 SchoolapCardPresence(
@@ -37,8 +78,8 @@ class CustomCard extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 10),
-            SCardWithImage(
+            const SizedBox(height: 10),
+            const SCardWithImage(
                 label: "Écrire un communiquer",
                 imagePath: "assets/images/pen.png"),
           ],
