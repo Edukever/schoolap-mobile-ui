f68ffb4fb1fa71f4ebb1b658a1f474f47b8d518c
osee-schoolap <osee.masuaku@schoolap.org>
2024-04-29 13:27:56 +0100

    Update sp_dropdown_styled.dart

--- lib/src/widgets/app_field/dropdowns/sp_dropdown_styled.dart
+++ lib/src/widgets/app_field/dropdowns/sp_dropdown_styled.dart
@@ -62,55 +62,64 @@ class SPDropDownStyled<T, R> extends StatelessWidget {
           valueTransformer: valueTransformer,
           focusNode: focusNode,
           builder: (state) {
-            return DropdownButtonFormField2<R>(
-              value: state.value,
-              items: DropdownMenuItemGenerator<T, R>(
-                items: items,
-              ).generate(getName: getName, getValue: getValue),
-              dropdownStyleData: dropdownStyleData ??
-                  DropdownStyleData(
-                    maxHeight: 200,
-                    padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 8.0),
-                    elevation: 0,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black),
-                      borderRadius: const BorderRadius.all(Radius.circular(14)),
-                    ),
-                    scrollbarTheme: ScrollbarThemeData(
-                      //   radius: const Radius.circular(40),
-                      thickness: MaterialStateProperty.all(6),
-                      thumbVisibility: MaterialStateProperty.all(true),
-                    ),
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DropdownButtonFormField2<R>(
+                  value: state.value,
+                  items: DropdownMenuItemGenerator<T, R>(
+                    items: items,
+                  ).generate(getName: getName, getValue: getValue),
+                  dropdownStyleData: dropdownStyleData ??
+                      DropdownStyleData(
+                        maxHeight: 200,
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 8.0),
+                        elevation: 0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black),
+                          borderRadius: const BorderRadius.all(Radius.circular(14)),
+                        ),
+                        scrollbarTheme: ScrollbarThemeData(
+                          //   radius: const Radius.circular(40),
+                          thickness: MaterialStateProperty.all(6),
+                          thumbVisibility: MaterialStateProperty.all(true),
+                        ),
+                      ),
+                  isExpanded: true,
+                  decoration: InputDecoration(
+                    prefixIcon: prefix,
+                    fillColor: backgroundColor,
+                    filled: backgroundColor != null,
+                    contentPadding: const EdgeInsets.only(right: 10),
+                    border: border ??
+                        OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                    // Add more decoration..
                   ),
-              isExpanded: true,
-              decoration: InputDecoration(
-                prefixIcon: prefix,
-                fillColor: backgroundColor,
-                filled: backgroundColor != null,
-                contentPadding: const EdgeInsets.only(right: 10),
-                border: border ??
-                    OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                // Add more decoration..
-              ),
-              iconStyleData: iconStyleData,
-              buttonStyleData: buttonStyleData,
-              menuItemStyleData: menuItemStyleData ??
-                  MenuItemStyleData(
-                    padding: EdgeInsets.zero,
-                    customHeights: DropdownMenuItemGenerator<T, R>(items: items).calculateItemHeights,
+                  iconStyleData: iconStyleData,
+                  buttonStyleData: buttonStyleData,
+                  menuItemStyleData: menuItemStyleData ??
+                      MenuItemStyleData(
+                        padding: EdgeInsets.zero,
+                        customHeights: DropdownMenuItemGenerator<T, R>(items: items).calculateItemHeights,
+                      ),
+                  hint: SPText(
+                    placeHolder,
+                    fontSize: 14,
+                    color: Colors.grey.withOpacity(0.4),
                   ),
-              hint: SPText(
-                placeHolder,
-                fontSize: 14,
-                color: Colors.grey.withOpacity(0.4),
-              ),
-              onChanged: (value) {
-                state.didChange(value);
-                onChanged?.call(value);
-              },
+                  onChanged: (value) {
+                    state.didChange(value);
+                    onChanged?.call(value);
+                  },
+                ),
+                if (state.hasError) ...[
+                  SizedBox(height: 5),
+                  SPText(state.errorText ?? '', color: AppTheme.of(context).colors.rouge),
+                ]
+              ],
             );
           },
         ),
