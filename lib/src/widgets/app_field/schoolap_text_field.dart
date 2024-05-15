9ccfef9c2b4c28e44c8198c4e45c6c2899ac33f0
Johnny Andedi <johnny.yetsha@schoolap.org>
2024-05-15 15:58:26 +0200

    Refactor the SPTextField widget by adding separate methods for building label and text field widgets. Commit Description:  - This MR refactors the SPTextField widget by separating the building of the label and text field widgets into their own methods for better organization and readability.

--- lib/src/widgets/app_field/schoolap_text_field.dart
+++ lib/src/widgets/app_field/schoolap_text_field.dart
@@ -6,65 +6,27 @@ enum AppTextFieldType { password }
 
 /// A custom text field widget for the app.
 class SPTextField<T> extends StatelessWidget {
-  /// The name of the text field.
   final String name;
-
-  /// The placeholder text for the text field.
   final String placeHolder;
-
-  /// The border radius of the text field.
   final BorderRadius? borderRadius;
-
-  /// The label text for the text field.
   final String? label;
-
-  /// The validator function for the text field.
   final String? Function(String?)? validator;
-
-  /// The suffix widget for the text field.
   final Widget? suffix;
-
-  /// The prefix widget for the text field.
   final Widget? prefix;
-
-  /// The maximum number of lines for the text field.
   final int? maxLines;
-
-  /// The type of the text field.
   final AppTextFieldType? type;
-
-  /// Whether the text field should obscure the text.
   final bool obscureText;
-
-  /// The hint style for the text field.
   final TextStyle? hintStyle;
-
-  /// Whether the text field is read-only.
   final bool readOnly;
-
-  /// The initial value for the text field.
   final String? initialValue;
-
-  /// The keyboard type for the text field.
   final TextInputType? keyboardType;
-
-  /// The onChanged callback for the text field.
   final void Function(String?)? onChanged;
-
-  /// The value transformer for the text field.
   final dynamic Function(String?)? valueTransformer;
-
-  /// The text capitalization for the text field.
   final TextCapitalization textCapitalization;
-
   final TextEditingController? controller;
-
   final void Function()? onEditingComplete;
-
   final Color? backgroundColor;
 
-  /// Creates a new instance of `SPTextField`.
-  ///
   const SPTextField({
     Key? key,
     required this.name,
@@ -92,6 +54,27 @@ class SPTextField<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (label != null) _buildLabel(),
+        _buildTextField(),
+      ],
+    );
+  }
+
+  Widget _buildLabel() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: SPText(
+        label!,
+        fontSize: 14.0,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _buildTextField() {
     final inputDecoration = SPCustomInputDecoration(
       suffixIcon: suffix,
       prefixIcon: prefix,
@@ -100,48 +83,21 @@ class SPTextField<T> extends StatelessWidget {
       backgroundColor: backgroundColor,
     );
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (label != null)
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: SPText(
-              label!,
-              fontSize: 14.0,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        if (type == AppTextFieldType.password)
-          FormBuilderTextField(
-            name: name,
-            readOnly: readOnly,
-            controller: controller,
-            initialValue: initialValue,
-            obscureText: obscureText,
-            validator: validator,
-            decoration: inputDecoration,
-            onChanged: onChanged,
-            valueTransformer: valueTransformer,
-            textCapitalization: textCapitalization,
-            onEditingComplete: onEditingComplete,
-          )
-        else
-          FormBuilderTextField(
-            name: name,
-            initialValue: initialValue,
-            readOnly: readOnly,
-            controller: controller,
-            maxLines: maxLines ?? 1,
-            validator: validator,
-            keyboardType: keyboardType,
-            decoration: inputDecoration,
-            textCapitalization: textCapitalization,
-            onChanged: onChanged,
-            valueTransformer: valueTransformer,
-            onEditingComplete: onEditingComplete,
-          ),
-      ],
+    return FormBuilderTextField(
+      name: name,
+      readOnly: readOnly,
+      controller: controller,
+      initialValue: initialValue,
+      obscureText: type == AppTextFieldType.password ? obscureText : false,
+      validator: validator,
+      decoration: inputDecoration,
+      onChanged: onChanged,
+      valueTransformer: valueTransformer,
+      textCapitalization: textCapitalization,
+      onEditingComplete: onEditingComplete,
+      maxLines: type == AppTextFieldType.password ? 1 : maxLines ?? 1,
+      keyboardType: type == AppTextFieldType.password ? TextInputType.text : keyboardType,
     );
   }
 }
+
